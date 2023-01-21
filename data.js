069d3de44791215b330b8c754b4e415541ea2866
ThurpatiNainesh <104067195+ThurpatiNainesh@users.noreply.github.com>
2023-01-21 19:22:07 +0530

    commit

---
+++ data.js
@@ -0,0 +1,15 @@
+let Users=[
+	{
+		userId : "054",
+		name : "John Doe",
+		tags : ["New", "Top5", "Contributor"],
+		isAdmin : true
+	},
+	{
+		userId : "034",
+		name : "Joanna Croft",
+		tags : ["Legend", "Composer"],
+		isAdmin : true
+	},
+]
+module.exports={Users}
---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const app = express()
+let { Users } = require('./data')
+
+
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
+
+
+
+app.put('/user', (req, res) => {
+    const query = req.query.tags
+
+  if (!query) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no person with id ${id}` })
+  }
+   let result=[]
+   let count=0
+         Users.map((person) => {
+    if (person.isAdmin === true && !person.tags.includes(query)) {
+      person.tags.push(query)
+      result.push(person.userId)
+          count++
+          console.log(person.tags)
+    }
+  })
+  
+  res.status(200).json({ success: true, data: {updatedUser:result,count:count}})
+})
+
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
+})
